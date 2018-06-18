a540e3fa68d5870e7b4158ebe9abacf90ebf999c
eyana-rc <eyana.mallari@ringcentral.com>
2018-06-18 01:05:04 -0700

    clean raw data code

---
+++ cleandata.py
@@ -0,0 +1,34 @@
+import os
+import numpy as np
+import pandas as pd
+import csv
+from math import cos, asin, sqrt
+from timeit import default_timer as timer
+from pathlib import Path
+from datetime import datetime
+
+
+# ------------------------------------------
+# ----------- Inputs  -------------
+# ------------------------------------------
+
+FILE_AIRPORTS  = "/Users/eyana.mallari/Projects-Local/py_airports/Input/world-airports.csv"
+
+
+def cleanRaw():
+    df = pd.read_csv(FILE_AIRPORTS,encoding = "ISO-8859-1")
+
+    print(df.columns)
+    df_filter = df[(df['iso_country']=='US') & (df['type'].isin(['large_airport','medium_airport','small_airport']))]
+
+    df_filter['iso_state'] = df_filter['iso_region'].str.split('-').str[1]
+    print(df_filter.sample(10))
+    [print(x) for x in df_filter['type'].unique()]
+    [print(x) for x in df_filter['iso_country'].unique()]
+    [print(x) for x in df_filter['iso_state'].unique()]
+
+    return 
+
+
+
+cleanRaw()
---
+++ logs.md
@@ -0,0 +1,15 @@
+1. Input: http://ourairports.com/data/
+
+
+2. Data Cleaning - Pandas
+
+Filters
+
+iso_country =  US
+type =
+large_airport
+medium_airport
+small_airport
+
+
+3.
